244b0368523115345d1072b3c8f90c8adf0ed597
Pratikthor19 <pratikgthorat92@gmail.com>
2023-10-14 17:22:21 +0530

    Post API Created

---
+++ Telecomp/src/main/java/com/velocity/serviceimpl/NetworkFunctionServiceImpl.java
@@ -0,0 +1,25 @@
+package com.velocity.serviceimpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.velocity.model.NetworkFunction;
+import com.velocity.repository.NetworkFunctionRepository;
+import com.velocity.service.NetworkFunctionService;
+@Service
+public class NetworkFunctionServiceImpl implements NetworkFunctionService{
+	@Autowired
+    private NetworkFunctionRepository networkfunctionrepository;
+	@Override
+	public NetworkFunction getNetworkFunctionById(Integer id) {
+		NetworkFunction netfuns=networkfunctionrepository.getNetworkFunctionById(id);
+		return netfuns;
+		
+	}
+	@Override
+	public NetworkFunction saveNetworkFunction(NetworkFunction networkfunction) {
+		NetworkFunction net = networkfunctionrepository.save(networkfunction);
+		return net;
+
+	}
+}
